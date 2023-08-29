9274621dc9968d13f826e3e70319185cd4b83335
JeonghwanSa <jeonghwansa@jobis.co>
2023-08-29 16:53:33 +0900

    fix typos

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_s3_bucket_ownership_controls" "bucketownership" {
     object_ownership = "BucketOwnerEnforced"
   }
 }
-resource "aws_s3_bucket_public_access_block" "publcaccess_block" {
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
   bucket = aws_s3_bucket.codepipeline_bucket.id
 
   block_public_acls       = true
